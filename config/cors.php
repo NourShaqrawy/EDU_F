9f344997524dd62c73b98a56457d8d7c3cbf6bcf
NourShaqrawy <shkrawysaleh037@gmail.com>
2025-08-07 21:19:12 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,18 +3,19 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://edu-f.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-XSRF-TOKEN'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
+
